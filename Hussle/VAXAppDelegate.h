fe3296484e99979a0d617d569e3e1f35c338d16a
xabra <adam.brailove@gmail.com>
2014-07-06 09:18:11 -0700

    Initial Commit

---
+++ Hussle/VAXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VAXAppDelegate.h
+//  Hussle
+//
+//  Created by Adam Brailove on 7/6/14.
+//  Copyright (c) 2014 Vaxis Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VAXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HussleTests/HussleTests.m
@@ -0,0 +1,34 @@
+//
+//  HussleTests.m
+//  HussleTests
+//
+//  Created by Adam Brailove on 7/6/14.
+//  Copyright (c) 2014 Vaxis Technologies. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HussleTests : XCTestCase
+
+@end
+
+@implementation HussleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
